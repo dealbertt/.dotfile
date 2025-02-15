d62c770e7b4e98691ab38c2b6baab1e61c24ffa0
dealbertt <dealbertignacio@gmail.com>
2025-02-15 23:28:24 +0100

    Config of home pc with fedora and wayland

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,9 +1,7 @@
 require("dealbertt")
+require("config.lazy")
 vim.api.nvim_set_hl(0, 'Normal', { bg = '#000000' })
-vim.o.termguicolors = true
--- When entering visual mode, automatically select the entire line
-vim.api.nvim_set_keymap('v', '<C-v>', 'V', { noremap = true, silent = true })
-require("oil").setup()
+--require("oil").setup()
 
 
 -- GRAY #1E1E1E
