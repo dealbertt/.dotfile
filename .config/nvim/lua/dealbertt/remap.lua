6af343234121a8c7b4c6e8ff6de351ba94fa871e
dealbertt <dealbertignacio@gmail.com>
2025-02-17 23:24:04 +0100

    Added some remaps to nvim

--- .config/nvim/lua/dealbertt/remap.lua
+++ .config/nvim/lua/dealbertt/remap.lua
@@ -2,3 +2,6 @@ vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv","<cmd>Oil<CR>", { noremap = true, silent = true })
 vim.keymap.set("n", "<leader>y","+y")
 vim.keymap.set("v", "<leader>y","+y")
+
+vim.keymap.set("n","<C-d","<C-d>zz")
+vim.keymap.set("n","<C-u","<C-u>zz")
