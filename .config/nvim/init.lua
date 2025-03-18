62bfe78ebea5d6fc60801b5d3fa293b44c690ac0
dealbertt <dealbertignacio@gmail.com>
2025-03-18 23:05:49 +0100

    Updated config 18/03/25 transparent alacritty window

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -3,6 +3,11 @@ require("config.lazy")
 vim.cmd.colorscheme "rose-pine"
 vim.o.background = 'dark'
 vim.api.nvim_set_hl(0, 'Normal', { bg = '#000000' })
+vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
+vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
+vim.api.nvim_set_hl(0, 'FloatBorder', { bg = 'none' })
+vim.api.nvim_set_hl(0, 'Pmenu', { bg = 'none' })
+
 --require("oil").setup()
 
 
