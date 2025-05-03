e4b4ca0ded2bc6b24393d67134358a4ccb45479a
dealbertt <dealbertignacio@gmail.com>
2025-05-03 22:14:50 +0200

    Second monitor back to horizontal

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,8 +1,8 @@
 require("dealbertt")
 require("config.lazy")
-vim.cmd.colorscheme "rose-pine"
-vim.o.background = 'dark'
-vim.api.nvim_set_hl(0, 'Normal', { bg = '#000000' })
+--vim.cmd.colorscheme "rose-pine"
+vim.cmd.colorscheme "tokyonight-storm"
+--vim.cmd[[colorscheme tokyonight-storm]]
 vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
 vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
 vim.api.nvim_set_hl(0, 'FloatBorder', { bg = 'none' })
