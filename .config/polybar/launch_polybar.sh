1a14657c9a1aa349da6abb0e139a944d3cce5263
dealbertt <dealbertignacio@gmail.com>
2026-07-03 13:47:43 +0200

    Added the wrong files on the previous commit, hehe

--- .config/polybar/launch_polybar.sh
+++
@@ -1,9 +0,0 @@
-killall -q polybar
-
-if type "xrandr"; then
-  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    MONITOR=$m polybar --config=~/.config/polybar/config.ini --reload example &
-  done
-else
-  polybar --config=~/.config/polybar/config.ini --reload example &
-fi
